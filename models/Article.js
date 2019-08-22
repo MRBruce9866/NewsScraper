6cfe7e94e7c9de9d218d957fbb3153d2f1c7d13f
Mark <mrbruce9866@gmail.com>
2019-08-22 14:33:01 -0400

    articles are now being saved to the mongo db

--- models/Article.js
+++ models/Article.js
@@ -3,14 +3,29 @@ const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
   title: {
-    type: String,
+    type: Array,
     required: true
   },
 
   link: {
     type: String,
     required: true
-  }
+  },
+
+  image: {
+    type: String,
+    required: false
+  },
+
+  snippet: {
+    type: String,
+    required: true
+  },
+
+  reviews: [{
+    type: Schema.Types.ObjectId,
+    ref: "Review"
+  }]
 });
 
 const Article = mongoose.model("Article", ArticleSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,33 +4,50 @@ const cheerio = require("cheerio");
 
 
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.get("/scrape", function(req, res){
+    app.get("/api/scrape", function (req, res) {
 
-    axios.get("https://www.tmz.com/").then((response) => {
-        const articles = [];
-
-        const $ = cheerio.load(response.data);
-        $(".blogroll .article").each((index,data) => {
-            if(data){
-                const article = {};
-                article.title = [];
-                $(data).children(".article__header").find(".article__header-title").children("span").each((index, data) => {
-                    article.title.push($(data).text().trim());
-                });
-                article.link = $(data).children(".article__header").children("a").attr("href");
-                article.image = $(data).children(".article__blocks").find("img").attr("src");
-                article.snippet = $(data).children(".article__blocks").children("section:nth-child(2)").text().trim();
-                articles.push(article);''
-            }
-            
-        })
-
-        
+        scrapeTMZ(data => {
+            const articles = data;
+            console.log(articles)
+            res.status(200).end();
+        });
 
     })
+
+    app.get("/api/articles", function (req, res) {
+
+        db.Article.find({}).then(data => {
+            res.json(data);
+        })
 
     })
 
 }
+
+function scrapeTMZ(cb) {
+    axios.get("https://www.tmz.com/").then((response) => {
+        const articles = [];
+        db.Article.remove({}, () => {
+            const $ = cheerio.load(response.data);
+            $(".blogroll .article").each((index, data) => {
+                if (data) {
+                    const article = {};
+                    article.title = [];
+                    $(data).children(".article__header").find(".article__header-title").children("span").each((index, data) => {
+                        article.title.push($(data).text().trim());
+                    });
+                    article.link = $(data).children(".article__header").children("a").attr("href");
+                    article.image = $(data).children(".article__blocks").find("img").attr("src");
+                    article.snippet = $(data).children(".article__blocks").children("section:nth-child(2)").text().trim();
+                    articles.push(article);
+                    db.Article.create(article).then(data => console.log(data)).catch(err => console.log(err));
+                }
+
+            })
+            // console.log(articles);
+            cb(articles);
+        });
+    })
+};
