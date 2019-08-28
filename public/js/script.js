41cc4e3053a2ed71a029954bf930d9d9e3066552
Markus Bruce <mrbruce9866@gmail.com>
2019-08-27 21:43:35 -0400

    fixed api calls

--- public/js/script.js
+++ public/js/script.js
@@ -25,16 +25,24 @@ $(document).on("click", ".articleContainer .article", function (event) {
         $("#articleModal .articleDisplay").prepend($article);
         $("#articleModal").modal("show")
 
+    })
 
 
-        $("#formSubmit").on("click", (event) => {
-            if($("#inputNote").val().trim()){
-                $.post(`/api/note/create/${response._id}`, {
-                    message: $("#inputNote").val().trim()
-                }).then((res) => {
-                    $("#articleModal .notes").empty();
-                    $("#inputNote").val("");
-                    response.notes.push(res)
+})
+
+
+$("#formSubmit").on("click", (event) => {
+    
+    let id = $(".articleDisplay .article").attr("data-id");
+    if($("#inputNote").val().trim()){
+        $.post(`/api/note/create/${id}`, {
+            message: $("#inputNote").val().trim()
+        }).then((res) => {
+
+            $.getJSON(`/api/articles/${id}`).then((response) => {
+                console.log(response);
+                $(".notes").empty();
+                if(response.notes.length > 0){
                     response.notes.forEach((note, index) => {
                         let $div = $("<div>").attr("class", "articleNote");
                         let $row = $("<div>").attr("class", "row").appendTo($div);
@@ -42,26 +50,14 @@ $(document).on("click", ".articleContainer .article", function (event) {
                         $row.append($("<div>").attr("class", "col-10 text-center articleNoteContent").text(`${note.message}`));
                         $(".notes").append($div);
                     });
-                })
-            }
+                }
+            })
             
-
         })
-
-    })
-
+    }
+    
 
 })
-
-$(document).click((event) => {
-    //if you click on anything except the modal itself or the "open modal" link, close the modal
-    if (!$(event.target).closest("#articleModal").length) {
-        $("#articleModal .articleDisplay").empty();
-    $("#articleModal .links").empty();
-    $("#articleModal .notes").empty();
-        $("#articleModal").hide();
-    }
-  });
 
   $("#scrapeBtn").on("click", (event) => {
     $.get("/api/scrape").then(() =>{
